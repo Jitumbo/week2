9f767cfd30b16e030537b1e08e63d33ee0ef8375
Jitumbo <jacob.nyaichowa@student.moringaschool.com>
2023-02-14 21:50:47 +0300

    adding the variables and main formula

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 var dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+function akanName (gender,day,month,year){
+    var gen = parseInt(gender);
+    var mwaka = parseInt(year);
+    var mwezi = parseInt(month);
+    var siku = parseInt(day);
+    cc = year.slice(0,1);
+    yy = year.slice(2,3);
+    dayWeek = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mwezi+1)/10)) + siku )% 7;
+
+
+
+
+
+
+
+}
